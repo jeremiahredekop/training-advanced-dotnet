d1435c2f1f98a91eae401ef5a7a41c08ed57831e
Jeremiah Redekop <jeremiah@geniuscode.net>
2021-06-30 10:02:54 -0700

    finished

--- Scheduling/Application/OverbookingProcessManager.cs
+++ Scheduling/Application/OverbookingProcessManager.cs
@@ -10,22 +10,38 @@ namespace Scheduling.Domain.Application
 {
     public class OverbookingProcessManager : EventHandler
     {
+        public const int MaxVisitThreshold = 3;
+
         public OverbookingProcessManager(IBookedSlotsRepository bookedSlotRepository, int bookingLimitedPerPatient,
             ICommandStore commandStore, Func<Guid> idGenerator)
         {
             When<SlotScheduled>(async (e, m) =>
             {
-                
+                await bookedSlotRepository.AddSlot(new BookedSlot()
+                {
+                    Id = e.SlotId.ToString(),
+                    DayId = e.DayId,
+                    IsBooked = false,
+                    Month = e.SlotStartTime.Month
+                });
             });
 
             When<SlotBooked>(async (e, m) =>
             {
+                await bookedSlotRepository.MarkSlotAsBooked(e.SlotId.ToString(), e.PatientId);                
+                var slot = await bookedSlotRepository.GetSlot(e.SlotId.ToString());
+                var bookings = await bookedSlotRepository.CountByPatientAndMonth(e.PatientId, slot.Month);
 
+                if (bookings > MaxVisitThreshold)
+                {
+                    await commandStore.Send(new CancelSlotBooking(e.DayId, e.SlotId, "overbooked"),
+                        new CommandMetadata(m.CorrelationId, m.CausationId));
+                }
             });
 
             When<SlotBookingCancelled>(async (e, m) =>
             {
-                
+                await bookedSlotRepository.MarkSlotAsAvailable(e.SlotId.ToString());
             });
         }
     }
